c9f09c618faf3ae8247b9270ab10f0e28687c15a
Sombody101 <evansheyborne@gmail.com>
2024-01-04 17:27:06 -0700

    Ownsership Transfer

---
+++ Utilities/YamlExtractor.Net/YamlExtractor.Net/Misc/Qol.cs
@@ -0,0 +1,25 @@
+﻿namespace YamlExtractor.Net.Misc;
+
+internal static class Qol
+{
+    /// <summary>
+    /// Return the pluralization for a word if <paramref name="count"/> <see langword="is not"/> 1
+    /// </summary>
+    /// <param name="count"></param>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public static string Pluralize(int count, string str)
+        => count == 1
+            ? ""
+            : str;
+
+    /// <inheritdoc/>
+    public static char Pluralize(int count, char chr)
+    => count == 1
+        ? '\0'
+        : chr;
+
+    /// <inheritdoc/>
+    public static string Pluralize(this string l_str, int count, string str)
+        => $"{l_str}{Pluralize(count, str)}";
+}
